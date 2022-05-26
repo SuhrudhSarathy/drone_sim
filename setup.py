e22a3755f5a37adcf9d4701cbbd06ec6bfe05708
Suhrudh <suhrudhsarathy@gmail.com>
2022-05-26 20:38:20 +0530

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,22 @@
-from setuptools import setup, find_namespace_packages
-with open(file="README.md", mode="r") as readme_handle:
-    long_description = readme_handle.read()
-requirements = open('requirements.txt').read().splitlines()
+from setuptools import setup
+from os.path import join
+import os
+
+project_name = "drone_sim"
+
 setup(
-    # Define the library name
-    name='Drone_Simulator',
-
-    # Repository's author's name
-    author='Suhrudh Sarathy',
-
-    # Email id of the author
-    author_email='suhrudhsarathy@gmail.com',
-
-    # URL to thr repository
-    url='https://github.com/SuhrudhSarathy/drone_sim.git',
-
-    # A short desciprtion to the package
-    description='A simple drone dynamics simulation written in python.',
-    long_description=long_description,
-
-    # Defined all the packages to be installed
-    packages=find_namespace_packages(include=['drone_sim.*']),
-
-    # Defined as Major Version=0, Minor-Version=1, Maintainance-version=0
-    version='0.1.0',
-
-    # the library required in order to run
-    install_requires=requirements,
-
-    # includes all the data in the package folder
-    include_package_data=True,
+    name=project_name,
+    version="0.1.0",
+    url="https://github.com/SuhrudhSarathy/drone_sim",
+    author="Suhrudh Sarathy",
+    author_email="suhrudhsarathy@gmail.com",
+    description="Light-weight Simulator for Drones",
+    license="MIT",
+    packages=[
+        project_name,
+        join(project_name, "gym"),
+        join(project_name, "sim"),
+        join(project_name, "viz"),
+    ],
+    zip_safe=True
 )
